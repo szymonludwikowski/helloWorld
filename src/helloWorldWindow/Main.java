eb20e1d85dcff47cd32f907126289267d741c046
szymonludwikowski <sz.ludwikowski@gmail.com>
2017-01-29 19:19:26 +0100

    3rd commit

---
+++ src/helloWorldWindow/Main.java
@@ -0,0 +1,12 @@
+package helloWorldWindow;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "Hello World!");
+	}
+
+}
